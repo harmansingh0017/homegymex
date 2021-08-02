f6480f0e29213989ef766f0ebdf0b87d742c6349
sing0017 <sing0017@edu.easj.dk>
2021-08-02 01:28:14 -0400

    y

--- js/harman.js
+++ js/harman.js
@@ -7,3 +7,40 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  function openLoginModal() {
+    showLoginForm();
+    setTimeout(function () {
+      $("#loginModal").modal("show");
+    }, 230);
+  }
+  function showLoginForm() {
+    $("#loginModal .registerBox").fadeOut("fast", function () {
+      $(".loginBox").fadeIn("fast");
+      $(".register-footer").fadeOut("fast", function () {
+        $(".login-footer").fadeIn("fast");
+      });
+
+      $(".modal-title").html("Login with");
+    });
+    $(".error").removeClass("alert alert-danger").html("");
+  }
+  function showRegisterForm(){
+$('.loginBox').fadeOut('fast',function(){
+    $('.registerBox').fadeIn('fast');
+    $('.login-footer').fadeOut('fast',function(){
+        $('.register-footer').fadeIn('fast');
+    });
+    $('.modal-title').html('Register with');
+}); 
+$('.error').removeClass('alert alert-danger').html('');
+   
+}
+
+$(function(){
+  $("#nav-placeholder").load("nav.html");
+});
+
+$(function(){
+  $("#login-placeholder").load("login.html");
+});
